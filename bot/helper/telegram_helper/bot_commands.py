24559f09a8b81c67eb2f5d73f439d33f31344753
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-02-15 11:38:43 +0530

    adding speed check command

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -7,6 +7,7 @@ class _BotCommands:
         self.CancelMirror = 'cancel'
         self.CancelAllCommand = 'cancelall'
         self.ListCommand = 'list'
+        self.SpeedCommand = 'speed'
         self.StatusCommand = 'status'
         self.AuthorizeCommand = 'authorize'
         self.UnAuthorizeCommand = 'unauthorize'
