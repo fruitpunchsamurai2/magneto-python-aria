1789c4736ac4567b747d176b6b01af747f5f673a
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-02-15 11:36:50 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,7 +13,7 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
-from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, delete, speedtest
 
 
 @run_async
@@ -98,6 +98,8 @@ def bot_help(update, context):
 /{BotCommands.AuthorizeCommand}: Authorize a chat or a user to use the bot (Can only be invoked by owner of the bot)
 
 /{BotCommands.LogCommand}: Get a log file of the bot. Handy for getting crash reports
+
+/{BotCommands.SpeedCommand} : Check Internet Speed Of The Host
 
 '''
     sendMessage(help_string, context.bot, update)
