8cdc25d66efcea5a3fb25888536242206479d66c
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-02-16 15:01:20 +0530

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -14,6 +14,6 @@ export MAX_CONCURRENT_DOWNLOADS=7
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
   --max-connection-per-server=16 --rpc-max-request-size=1024M \
   --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
-   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=600 \
+   --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED --bt-stop-timeout=1200 \
    --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
    --bt-tracker=$tracker --bt-max-peers=0 --peer-id-prefix=-TR2770- --user-agent=Transmission/2.77 --peer-agent=Transmission/2.77 \
