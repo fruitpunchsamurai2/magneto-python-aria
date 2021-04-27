615ed9edb494596530bb48aa92cb55d19de613b3
xd003 <singhbadal04@gmail.com>
2021-04-27 09:00:03 +0530

    Delete old TrackList before appending them at every restart (#19)
    * Delete old TrackList before appending them at every restart
    
    * Clean aria.conf through string matching instead of line Number

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 pip3 -qq install --upgrade yt-dlp
+sed -n -i '/max-concurrent-downloads/q;p' /app/aria.conf
 tracker_list=`curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',' | cat`
 echo -e "\nmax-concurrent-downloads=4\nbt-tracker=$tracker_list" >> /app/aria.conf
 aria2c --conf-path=/app/aria.conf
