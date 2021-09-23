184d5ae5265e93614ebdfa19fa004f31d9f4fe2f
Sunil Kumar <66400864+iamLiquidX@users.noreply.github.com>
2021-09-23 14:01:08 +0530

    version bump
    For build error fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (CWD / "README.md").read_text()
 
 setup(
     name='MirrorX',
-    version='6.0.2',
+    version='6.0.3',
     packages=find_packages(),
     long_description=README,
     long_description_content_type="text/markdown",
